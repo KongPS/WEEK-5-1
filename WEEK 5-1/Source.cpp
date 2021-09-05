c87b8cc78048b11962b5c3cd4bb4b4c236cb1bf5
ASUS <ASUS@LAPTOP-IKVEPC7K>
2021-09-05 21:43:28 +0700

    Add project files.

---
+++ WEEK 5-1/Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int x[10];
+	int sum = 0;
+	for (int n = 0; n <= 9; n++)
+	{
+		scanf_s("%d", &x[n]);
+		sum = sum + x[n];
+	}
+	for (int n = 0; n <= 9; n++)
+	{
+		printf("%d\n", x[n]);
+	}
+	sum = sum / 10;
+	printf("%d", sum);
+
+
+}
